fad1466f9b572c8a15c69f66c2bc6ed50ab9753e
Aasmakanwal <64689820+Aasmakanwal@users.noreply.github.com>
2020-07-17 21:33:21 +0500

    1st commit

---
+++ app.js
@@ -0,0 +1,291 @@
+// *****CHAPTER 38-42*****
+
+// Question # 1
+
+// function power(a , b){
+//     var result = 1;
+//     for(i = 1 ; i <= b ; i++){
+        
+//         result = result * a;
+//     }
+//     return result
+// }
+// document.write(power(3 , 3))
+
+// Question # 2
+
+// function leapYear(year){
+//     if (year % 4 == 0){
+//         document.write(year + " is a Leap Year")
+//     }else {
+//         document.write(year + " is not a Leap Year")
+//     }
+// }
+// year = prompt("Enter any year")
+// leapYear(year)
+
+
+// Question # 3
+
+// function areaOfTriangle(a, b ,c){
+//     var area;
+//     function S(){
+//        var s = (a + b + c)/2
+//        return s
+//     }
+//     var d = S()
+//     area = d*(d - a)*(d - b)*(d - c)
+//     return area
+//     // return d
+// }
+// document.write(areaOfTriangle(2, 3 ,4))
+
+// Question # 4
+
+// function result(mark1, mark2, mark3){
+//     function average(){
+//         var average = (mark1 + mark2 + mark3)/3
+//         return average
+//     }
+//     function percentage(){
+//         var percentage = (mark1 + mark2 + mark3)*100/150
+//         return percentage
+//     }
+//     // var average = average()
+//     // var percentage = percentage()
+//     document.write(average() + "<br>" ) 
+//     document.write(percentage())
+// }
+
+// var mark1 = parseInt(prompt("Enter Marks of First subject"))
+// var mark2 = parseInt(prompt("Enter Marks of Second subject"))
+// var mark3 = parseInt(prompt("Enter Marks of Third subject"))
+// result(mark1 , mark2, mark3)
+
+// Question # 5
+
+
+// function indexOf(){
+
+//     var word = prompt("Enter word or paragraph:")
+
+//     var character = prompt("Enter Character")
+
+//     for(i=0 ; i<= word.length ; i++){
+
+//         if(word[i] == character){
+
+//             document.write("Index of " + character + " is " + i)
+
+//             break
+//         }
+//     }
+// }
+
+// indexOf()
+
+// Question # 6
+
+// var sentence;
+
+// function removeVowels(sentence){
+//     var sen = sentence.toLowerCase()
+//     var ne = ""
+//     for(i = 0 ; i < sentence.length ; i++){
+
+//         if(sen[i] != "a" && sen[i] != "e" && sen[i] != "i" && sen[i] != "o" && sen[i] != "u"){
+//             ne += sentence[i]
+//         }
+//     }
+//     return ne
+// }
+
+// sentence = prompt("Enter a sentence:")
+// document.write(removeVowels(sentence))
+
+// Question # 7
+
+// var sentence = "Pleases read this application and give me gratuity"
+
+// function occuranceOfTwoVowels(sentence){
+//     var lower = sentence.toLowerCase()
+//     var count = 0
+//     var list =[]
+//     for(i = 0 ; i< sentence.length ; i++){
+//         var sen = ""
+//         var first = lower[i]
+//         switch(first){
+//             case "a":
+//                 sen += "a"
+//                 break
+//             case "e":
+//                 sen += "e"
+//                 break
+//             case "i":
+//                 sen += "i"
+//                 break
+//             case "o":
+//                 sen += "o"
+//                 break
+
+//             case "u":
+//                 sen += "u"
+//                 break
+//         }
+//         var second = lower[i+1]
+//         switch(second){
+//             case "a":
+//                 sen += "a"
+//                 break
+//             case "e":
+//                 sen += "e"
+//                 break
+//             case "i":
+//                 sen += "i"
+//                 break
+//             case "o":
+//                 sen += "o"
+//                 break
+
+//             case "u":
+//                 sen += "u"
+//                 break
+//         }
+
+//         if(sen.length == 2){
+//             count += 1
+//             list.push(sen)
+//         }        
+//     }
+    
+//     document.write("There are " +count+ " occurrences of any two vowels in succession <br>")
+//     document.write(list + "<br>")
+// }
+
+// sentence = prompt("Enter any sentence:")
+// occuranceOfTwoVowels(sentence)
+
+
+// Question # 8
+
+// function kilometerToMeter(distance){
+//     var meter = distance * 1000
+//     return meter
+// }
+
+// function kilometerToFeet(distance){
+//     var feet = distance * 3280.8399
+//     return feet
+// }
+
+// function kilometerToInch(distance){
+//     var inch = distance * 39370.0787
+//     return inch
+// }
+
+// function kilometerToCentimeter(distance){
+//     var cm= distance * 100000
+//     return cm
+// }
+
+// function distanceConversion(distance){
+//     meter =  kilometerToMeter(distance) 
+//     feet = kilometerToFeet(distance)
+//     inch = kilometerToInch(distance)
+//     cm = kilometerToCentimeter(distance)
+//     document.write("<b>Kilometer to Meter:</b> " + distance + " kilometer is equal to " +meter+ " meters<br>")
+//     document.write("<b>Kilometer to Feet:</b> " + distance + " kilometer is equal to " +feet+ " feets<br>")
+//     document.write("<b>Kilometer to Inches:</b> " + distance + " kilometer is equal to " +inch+ " inches<br>")
+//     document.write("<b>Kilometer to Centimeter:</b> " + distance + " kilometer is equal to " +cm+ " centimeters<br>")
+// }
+
+// var distance = parseInt(prompt("Enter distance in kilometer:"))
+// distanceConversion(distance)
+
+
+// Question # 9
+
+// var hours = prompt("enter working hours:")
+// if (hours > 40){
+//     hours = hours -40
+//     var pay = hours * 12
+// }
+
+// document.write("Over time pay: " +pay)
+
+// Question # 10
+
+// function currencyNotes(amount){
+
+//     var tens = amount[amount.length - 2] % 5
+//     var hundreds = amount.slice(0, amount.length - 2)
+//     if (amount[amount.length - 2] >= 5){
+//         var fifty = 1
+//     }else{
+//         var fifty = 0
+//     }
+//     document.write("You will have " + hundreds + " hundred notes " + fifty + " fifty note and " + tens + " ten notes")
+// }
+
+// var amount = prompt("Enter amount to withdraw:")
+// currencyNotes(amount)
+
+// document.write(tens + " " + hundreds)
+
+ 
+// *****CHAPTER 43-48*****
+
+// Question # 1
+
+// function link(){
+//     alert("This link is not available right now")
+// }
+
+// Question # 2
+
+// function link(){
+//     alert('Thanks for purchasing a phone from us')
+// }
+
+// Question # 3
+
+
+
+// Question # 4
+
+// function imgChange(){
+//    var img = document.getElementById("change")
+//    img.src = "images/img2.jfif"
+// }
+// function imgchange2(){
+//    var img = document.getElementById("change")
+//    img.src = "images/img1.jfif"
+// }
+  
+
+// Question # 5
+
+
+// function increase(){
+
+//   var count =  document.getElementById("counter")
+
+//   count.innerHTML++
+// }
+// function decrease(){
+
+//    var count =  document.getElementById("counter")
+ 
+//    count.innerHTML--
+//  }
+ 
+// *****CHAPTER 49-52*****
+
+// Question # 1
+
+function para(){
+
+var para = document.getElementById("para")
+
+para.innerHTML = " In previous assignment you have created a tabular data using javascript. Let’s modify that. Create a form which takes student’s details and show each student detail in table. Each row of table must contain a delete button and an edit button. On click on delete button entire row should be deleted. On click on edit button, a hidden form will appear with the values of that row."
+}
